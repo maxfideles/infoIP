0450ff0d9f711c71cf65ecba7a738dca19a75ae3
maxfideles <maxfideles24@gmail.com>
2023-06-02 15:48:00 -0300

    Initial Commit

---
+++ infoIP/infoIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  infoIPApp.swift
+//  infoIP
+//
+//  Created by Max Victor on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct infoIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
